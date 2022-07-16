3f41b5cc4f3e8c0907178594bf27e8cbf749be8d
Tom Witkowski <tom.witkowski@frischepost.de>
2022-07-16 17:25:19 +0200

    add fetching

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
-export default function handler(request, response) {
-    response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-    });
+import fetch from 'node-fetch';
+
+export default async function handler(request, response) {
+    const ip = request.query.ip;
+    const port = request.query.port;
+
+    if(!ip) {
+        return response.status(412).json({
+            error: 'ip missing',
+        });
+    }
+
+    if(!port) {
+        return response.status(412).json({
+            error: 'port missing',
+        });
+    }
+
+    try {
+        const json = await fetch(`http://${ip}:${port}/players.json`);
+        const players = await json.json();
+    } catch(e) {
+        return response.status(500).json(e);
+    }
+
+    return response.status(200).json(players);
 }
